cc0b276ba14e2287a3d1c03f619f030a77ee6cc9
SSA111 <Sesa111@gmail.com>
2015-05-12 18:32:50 +0200

    Fix SSASideMenu Implementation

--- Ridge/ViewController.swift
+++ Ridge/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+      
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
 
     override func didReceiveMemoryWarning() {
