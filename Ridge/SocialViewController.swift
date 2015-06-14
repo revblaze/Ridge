f4751be78ff20748758f4398484858e7916e5341
Justin Bush <jaybush7@gmail.com>
2015-06-14 16:17:34 -0400

    Fixed TwitterKit crash
    Storyboard issue fixed.

--- Ridge/SocialViewController.swift
+++ Ridge/SocialViewController.swift
@@ -9,29 +9,12 @@
 import UIKit
 import TwitterKit
 
-class SocialViewController: UITableViewController, TWTRTweetViewDelegate {
+class SocialViewController: TWTRTimelineViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
-
+        let client = Twitter.sharedInstance().APIClient
+        self.dataSource = TWTRUserTimelineDataSource(screenName: "fabric", APIClient: client)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
